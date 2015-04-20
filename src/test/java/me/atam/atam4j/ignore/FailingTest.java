c6208209bcab6ab084f55cc51fcf9e91485fe375
phillbarber <philliprbarber@googlemail.com>
2015-04-20 14:54:40 +0100

    Issue 18 - Added tests for AcceptanceTestsRunnerTask

---
+++ src/test/java/me/atam/atam4j/ignore/FailingTest.java
@@ -0,0 +1,15 @@
+package me.atam.atam4j.ignore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FailingTest {
+
+
+    @Test
+    public void testThatFails(){
+        assertTrue(false);
+    }
+
+}
