7657707e4bacf6af5e2993b36c7c2cb50297032b
Anurag Kapur <anurag@beancrunch.com>
2015-10-27 13:26:18 +0000

    Fix release trigger logic

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if  [[ $TRAVIS_BRANCH == 'master' && !$TRAVIS_PULL_REQUEST ]]
-then
+if [[ $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_BRANCH == 'master' ]]; then
     git remote add origin git@github.com:atam4j/atam4j.git
     mvn release:prepare release:perform -DskipTests=true -B
     mvn deploy -Prelease -Dgpg.defaultKeyring=false -Dgpg-keyname=BF230DCD -Dgpg.passphrase=$GPGKEY_PASSPHRASE -Dgpg.publicKeyring=~/pubring.gpg -Dgpg.secretKeyring=~/secring.gpg -DskipTests=true --settings ~/settings.xml
 fi
+
