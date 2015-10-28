6ba237448e4e6b3d52abd842313ed11577e91c34
Anurag Kapur <anurag@beancrunch.com>
2015-10-28 16:59:16 +0000

    Fix maven central release bug

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,6 @@
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST = "false" ]]; then
     git checkout master
     git remote add origin git@github.com:atam4j/atam4j.git
-    mvn release:prepare release:perform -DskipTests=true -B
-    mvn deploy -Prelease -Dgpg.defaultKeyring=false -Dgpg-keyname=BF230DCD -Dgpg.passphrase=$GPGKEY_PASSPHRASE -Dgpg.publicKeyring=~/pubring.gpg -Dgpg.secretKeyring=~/secring.gpg -DskipTests=true --settings ~/settings.xml
+    mvn release:prepare release:perform -DskipTests=true -B -Prelease -Dgpg.defaultKeyring=false -Dgpg-keyname=BF230DCD -Dgpg.passphrase=$GPGKEY_PASSPHRASE -Dgpg.publicKeyring=~/pubring.gpg -Dgpg.secretKeyring=~/secring.gpg -DskipTests=true --settings ~/settings.xml
 fi
 
