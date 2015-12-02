c0951a6c135c87b6239a7abe8a318822c63341d7
Anurag Kapur <anurag@beancrunch.com>
2015-12-02 15:57:22 +0000

    Removed unused var

--- atam4j/src/main/java/me/atam/atam4j/AcceptanceTestsRunnerTask.java
+++ atam4j/src/main/java/me/atam/atam4j/AcceptanceTestsRunnerTask.java
@@ -4,12 +4,8 @@ import me.atam.atam4j.health.AcceptanceTestsState;
 import me.atam.atam4j.logging.LoggingListener;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AcceptanceTestsRunnerTask implements Runnable {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(AcceptanceTestsRunnerTask.class);
 
     private final AcceptanceTestsState testsState;
     private final Class[] testClasses;
