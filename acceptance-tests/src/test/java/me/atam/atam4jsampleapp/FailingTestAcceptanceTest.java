e0e303222e257b8c7d05746fc86d97c2b02ae8ba
Phill <Phill>
2019-04-25 16:58:15 +0100

    add debugging

--- acceptance-tests/src/test/java/me/atam/atam4jsampleapp/FailingTestAcceptanceTest.java
+++ acceptance-tests/src/test/java/me/atam/atam4jsampleapp/FailingTestAcceptanceTest.java
@@ -1,14 +1,22 @@
 package me.atam.atam4jsampleapp;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import me.atam.atam4j.dummytests.FailingTest;
 import me.atam.atam4j.dummytests.PassingAndFailingTests;
+import me.atam.atam4j.logging.LoggingListener;
 import me.atam.atam4jdomain.IndividualTestResult;
 import me.atam.atam4jdomain.TestsRunResult;
 import me.atam.atam4jsampleapp.testsupport.AcceptanceTest;
 import me.atam.atam4jsampleapp.testsupport.Atam4jApplicationStarter;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
+
+import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
@@ -18,6 +26,21 @@ public class FailingTestAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void givenSampleApplicationStartedWithFailingTest_whenHealthCheckCalledAfterTestRun_thenFailuresMessageReceived(){
+
+        Logger logger = LoggerFactory.getLogger(LoggingListener.class);
+
+        ch.qos.logback.classic.Logger logbackLogger = (ch.qos.logback.classic.Logger) logger;
+        System.out.println("FailingTestAcceptanceTest effecitveLevel: " + logbackLogger.getEffectiveLevel());
+        System.out.println("FailingTestAcceptanceTest STDOUT appender: " + logbackLogger.getAppender("STDOUT"));
+        System.out.println("FailingTestAcceptanceTest STDOUT isInfoEnabled: " + logbackLogger.isInfoEnabled());
+        System.out.println("FailingTestAcceptanceTest STDOUT loggerCOntext: " + logbackLogger.getLoggerContext());
+        Iterator<Appender<ILoggingEvent>> appenderIterator = logbackLogger.iteratorForAppenders();
+        while(appenderIterator.hasNext()){
+            System.out.println("FailingTestAcceptanceTest appenderIterator.next: " + appenderIterator.next());
+        }
+
+
+
         //given
         dropwizardTestSupportAppConfig = Atam4jApplicationStarter.startApplicationWith(0, FailingTest.class, 1);
         //when
